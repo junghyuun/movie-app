86a2bd7390bf5dccfb244f36108d394d8a25ab97
junghyuun <anjjang23@gmail.com>
2021-04-13 17:29:04 +0900

    styling movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-function Movie({ id, year, title, summary, poster }) {
-  return <h5>{ title }</h5>
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title">{ title }</h3>
+        <h5 class="movie__year">{ year }</h5>
+        <p class="movie__summary">{ summary }</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
